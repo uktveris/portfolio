7950d19612e806ec281a9774e3ee3dfd37a41187
uktveris <marius.uktveris@gmail.com>
2025-02-09 15:18:19 +0200

    add more data

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,41 +1,43 @@
-import Card from './Card'
-import styles from './styles.module.css'
+import Card from "./Card";
+import styles from "./styles.module.css";
 
-const projects: {title: string, desc: string, technologies: string[], link: string}[] = [
-    {
-        "title": 'dahu',
-        "desc": 'Dahu is a color theme extension for Visual Studio Code. It allows users to generate color themes from an image, set colors for background and code syntax manually.',
-        // "imgPath": '../assets/AstronautWP.jpg.jpg',
-        "technologies": ['TypeScript', 'Node.js', 'Angular'],
-        "link": 'https://github.com/dahu-theme-generator/dahu'
-    },
-    // {
-    //     "title": 'Social media app',
-    //     "desc": 'A simple mobile application which allows users to take photos, post them and follow other users.',
-    //     // "imgPath": '../assets/AstronautWP.jpg.jpg',
-    //     "technologies": ['TypeScript', 'Kotlin', 'Node.js', 'PostgreSQL'],
-    //     "link": 'https://github.com/FontysVenlo/prj3-2023-android-09'
-    // },
-    // {
-    //     "title": 'Airline Information System',
-    //     "desc": 'A desktop application that simulates an airline information service. Allows users to add, remove and book flights, checks weather conditions on the flight destination.',
-    //     // "imgPath": '../assets/AstronautWP.jpg.jpg',
-    //     "technologies": ['Java', 'JavaFX', 'PostgreSQL'],
-    //     "link": 'https://github.com/FontysVenlo/prj2-2023-prj2-2023-13'
-    // }
-]
+const projects: {
+  title: string;
+  desc: string;
+  technologies: string[];
+  link: string;
+}[] = [
+  {
+    title: "dahu",
+    desc: "Dahu is a color theme extension for Visual Studio Code. It allows users to generate color themes from an image, set colors for background and code syntax manually.",
+    technologies: ["TypeScript", "Node.js", "Angular"],
+    link: "https://github.com/dahu-theme-generator/dahu",
+  },
+  {
+    title: "Cloud file sharing platform",
+    desc: "Cloud storage platform, having similar (yet more limited) functionality to general cloud file storage systems, such as Google Drive or ICloud. Built mostly using TypeScript, with well-known MERN stack.",
+    technologies: ["TypeScript", "MongoDB", "Express", "React", "Node.js"],
+    link: "https://github.com/uktveris/synterium-frontend",
+  },
+];
 
 export function Projects() {
-    return (
-        <section id='projects'>
-            <div className={styles.projectsContainer}>
-                <h2>Projects</h2>
-                <div className={styles.projects}>
-                    {projects.map((project, index) => (
-                        <Card key={index} title={project.title} desc={project.desc} technologies={project.technologies} link={project.link}/>
-                    ))}
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section id="projects">
+      <div className={styles.projectsContainer}>
+        <h2>Projects</h2>
+        <div className={styles.projects}>
+          {projects.map((project, index) => (
+            <Card
+              key={index}
+              title={project.title}
+              desc={project.desc}
+              technologies={project.technologies}
+              link={project.link}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
